4429672a22ea6062a2a4d5523c733ae69673d481
Ilya <tyulin600@gmail.com>
2024-04-28 23:20:51 +0500

    removed the auto-generated files from src and test and made the server start at port 5000

--- server/src/main.ts
+++ server/src/main.ts
@@ -3,6 +3,12 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  try {
+    await app.listen(5000);
+    console.log('Server is started at port 5000.');
+  } catch (error) {
+    console.log(error);
+  }
 }
+
 bootstrap();
